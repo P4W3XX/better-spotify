0e97bccd2c027d46cad5a6b00bd524ec1e46af11
Wiktor Murdzia <87718710+v1cttorr@users.noreply.github.com>
2025-04-05 21:29:57 +0200

    Update settings.py

--- backend/betterspotify/settings.py
+++ backend/betterspotify/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+'corsheaders.middleware.CorsMidddleware',
 ]
 
 ROOT_URLCONF = 'betterspotify.urls'
