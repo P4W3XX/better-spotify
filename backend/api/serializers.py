9889659fdbc3430b641d0c16cd3c980cdd69c827
Wiktor Murdzia <murdziawiktor@gmail.com>
2025-04-07 18:31:24 +0200

    feat: add album duration calculation and update album serializer fields

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,5 @@
+import datetime
+from django.db.models import Sum
 from rest_framework import serializers
 from .models import Artist, Album, Song
 from drf_spectacular.utils import extend_schema_field
@@ -19,9 +21,10 @@ class SongSerializer(serializers.ModelSerializer):
 class AlbumSerializer(serializers.ModelSerializer):
     # songs = SongSerializer(many=True, required=False, nested=True)
     songs = serializers.SerializerMethodField()
+    album_duration = serializers.SerializerMethodField()
     class Meta:
         model = Album
-        fields = ['id', 'title', 'album_type', 'artist', 'image', 'release_date', 'songs']
+        fields = ['id', 'title', 'album_type', 'artist', 'image', 'release_date', 'album_duration',  'songs']
 
     def __init__(self, *args, **kwargs):
         nested = kwargs.pop('nested', False)
@@ -33,11 +36,39 @@ class AlbumSerializer(serializers.ModelSerializer):
 
     @extend_schema_field(serializers.ListField)
     def get_songs(self, obj):
-        return SongSerializer(obj.songs, many=True, nested=True, context=self.context).data
+        return SongSerializer(obj.songs, many=True, nested=True, context=self.context, required=False).data
+    
+    def get_album_duration(self, obj):
+        total_time = datetime.timedelta(0)
+        for song in obj.songs.all():
+            total_time += song.duration
+            print(type(song.duration))
+
+        # formated_time = str(total_time).split(':')
+        # if formated_time[0] != '0':
+        #     return f"{formated_time[0]}:{formated_time[1]}:{formated_time[2]}"
+        # else:
+        #     return "00:00"
+
+        total_seconds = total_time.total_seconds()
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        seconds = total_seconds % 60
+
+        return f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
+
+        # return formated_time
+        # return sum(song.duration for song in obj.songs.all())
 
     def create(self, validated_data):
-        songs_data = validated_data.pop('songs')
+        try:
+            songs_data = validated_data.pop('songs')
+        except KeyError:
+            songs_data = []
+        
         album = Album.objects.create(**validated_data)
+        
+
         for song_data in songs_data:
             Song.objects.create(album=album, **song_data)
         return album
