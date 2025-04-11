620d835b7f66a6ab2de646e20663b775c2c8c54b
Paweł Sarzyński <pawexx102a@wp.pl>
2025-04-11 21:39:58 +0200

    fix Przema

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 import Image from 'next/image';
 
 export default function LoginPage() {
