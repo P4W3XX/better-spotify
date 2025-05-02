4e92373e8e647753e36cfbe4717b8782fed718f3
Paweł Sarzyński <pawexx102a@wp.pl>
2025-05-02 19:24:19 +0200

    feat: enhance search and song preview components with additional features and API integration

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,65 +1,206 @@
 'use client'
 
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
-import { motion } from "framer-motion";
+import { ArrowLeft, Search } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import PopularCover from "@/components/popular-cover";
 import { SongPreview } from "@/components/song-preview";
+import { useMediaQuery } from "usehooks-ts";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import axios from 'axios';
+import BestResult from "@/components/best-result";
+import TopSongs from "@/components/top-songs";
+
+interface SearchResultsProps {
+    results: { id: string, image: string, title: string, data_type: string, artist: number, username: string, songs: [{ id: number }], plays: number, duration: string, feats: [] }[];
+    count: number;
+}
 
 export default function SearchPage() {
     const [search, setSearch] = useState<string>("");
     const [isSearching, setIsSearching] = useState<boolean>(false);
+    const [isMounted, setIsMounted] = useState(false);
+    const [showLastSearches, setShowLastSearches] = useState(false);
+    const [searchResults, setSearchResults] = useState<SearchResultsProps>({ results: [], count: 0 });
+
+    const mobile = useMediaQuery("(max-width: 768px)");
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (isSearching) {
+            setTimeout(() => {
+                setShowLastSearches(true);
+            }, 300);
+        } else {
+            setShowLastSearches(false);
+        }
+    }, [isSearching]);
+
+
+    useEffect(() => {
+        if (search.length > 0) {
+            const fetchSearchResults = async () => {
+                try {
+                    const response = await axios.get(`http://127.0.0.1:8000/api/search/?q=${search}`);
+                    console.log(response.data.results);
+                    // Map through results to extract only the needed properties
+                    setSearchResults({
+                        results: response.data.results.map((item: {
+                            id: string;
+                            image: string;
+                            title: string;
+                            data_type: string;
+                            artist: number;
+                            username: string;
+                            songs?: [{ id: number }];
+                            plays?: number;
+                            duration?: string;
+                            feats?: [];
+
+                        }) => ({
+                            id: item.id,
+                            image: item.image,
+                            title: item.title,
+                            data_type: item.data_type,
+                            artist: item.artist,
+                            username: item.username,
+                            songs: item.songs,
+                            plays: item.plays,
+                            duration: item.duration,
+                            feats: item.feats,
+                        })),
+                        count: response.data.count,
+                    });
+                    console.log(searchResults);
+                } catch (error) {
+                    console.error("Error fetching search results:", error);
+                }
+            };
+            fetchSearchResults();
+        }
+    }, [search]);
+
+    if (!isMounted) return null;
+
+
 
     return (
         <main style={{
             overflowY: isSearching ? "hidden" : "auto",
-        }} className="w-full p-5 pb-[7rem] relative h-svh">
-            <div className="w-full h-[3rem] relative bg-black z-[999]">
-                <Input
-                    onFocus={() => setIsSearching(true)}
-                    onBlur={() => setIsSearching(false)}
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    className="w-full h-full font-medium pl-11 z-[100] rounded-2xl absolute top-0 left-0"
-                />
-                <div className="flex items-center absolute z-[100] gap-x-2 left-0 p-3 font-medium pointer-events-none w-full top-0 bottom-0 my-auto h-max">
-                    <Search className="opacity-40" />
-                    <motion.p
-                        animate={{
-                            opacity: search.length > 0 ? 0 : 1,
-                            y: search.length > 0 ? -10 : 0,
-                        }}
-                        className="text-white/40"
-                    >
-                        Search music, artists, albums, and playlists.
-                    </motion.p>
-                </div>
-            </div>
-            <div className="w-full mt-6 grid grid-cols-2 z-0 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 auto-rows-fr">
-                <PopularCover title="RAP" artist="travis" />
-                <PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" />
-                <PopularCover title="TRAP" artist="carti" />
-                <PopularCover title="DISCO POLO" artist="zenek" />
-            </div>
+        }} className="w-full pb-[7rem] px-5 relative h-svh">
             <motion.div initial={false} animate={{
-                opacity: isSearching ? 1 : 0,
-                pointerEvents: isSearching ? "all" : "none",
-            }} className=" w-full h-full left-0 absolute mb-[8rem] px-5 top-[5.5rem] bg-black/50 z-0 rounded-3xl">
-                <motion.div initial={false} animate={{
+                opacity: isSearching && search.length === 0 ? 1 : 0,
+                pointerEvents: isSearching && search.length === 0 ? "all" : "none",
+            }} className=" w-svw h-svh absolute left-0 bg-black/80 top-0 z-[99]" />
+            <div className="w-full h-[3rem] sticky py-10 items-center bg-background justify-center flex z-[999] top-0">
+                {mobile && (<motion.button initial={false} animate={{
+                    x: isSearching ? 0 : -100,
+                    width: isSearching ? "50px" : "0px",
+                    marginLeft: isSearching ? "1.25rem" : "0px",
                     opacity: isSearching ? 1 : 0,
-                    scale: isSearching ? 1 : 0.9,
                 }} transition={{
                     ease: "easeInOut",
-                }} className=" w-full h-1/2 origin-top pt-4 px-5 bg-black border border-zinc-800 rounded-3xl">
-                    <p className=" text-white font-semibold text-xl">
-                        Lastest searches
-                    </p>
-                    <div className="w-full h-full ">
-                        <SongPreview index={0} title="Song Title" artist="Artist Name" feats={[]} plays={1000} duration="3:45" isCover={true} id="1" />
+                }} className=" font-medium  hover:bg-zinc-800/80 z-[100] h-[40px] rounded-xl cursor-pointer flex items-center justify-center">
+                    <ArrowLeft size={25} className=" min-w-[25px] stroke-3" onClick={() => setIsSearching(false)} />
+                </motion.button>)
+                }
+                <div className=" relative w-full h-full flex items-center justify-center">
+                    <Input
+                        onFocus={() => setIsSearching(true)}
+                        onBlur={() => setIsSearching(false)}
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="w-full h-[3rem] font-medium !bg-background z-[102] pl-10 rounded-2xl"
+                    />
+                    <motion.div initial={false} animate={{
+                        scaleY: isSearching && search.length === 0 ? 1 : 0,
+                        opacity: isSearching && search.length === 0 ? 1 : 0,
+                    }} transition={{
+                        ease: "easeInOut",
+                        delay: isSearching && search.length === 0 ? 0.2 : 0,
+                    }} className=" w-full h-[27.5rem] pt-10 border origin-top bg-zinc-950 rounded-b-3xl z-[101] absolute top-0">
+                        <div className=" flex w-full items-center px-6 justify-between">
+                            <h1 className=" font-semibold text-xl">
+                                Lastest Searches
+                            </h1>
+                            <Button variant={"ghost"} className=" rounded-lg text-white/50 text-base">
+                                Reset
+                            </Button>
+                        </div>
+                        <div className=" mt-2 px-4 flex flex-col gap-y-1">
+                            {showLastSearches && (
+                                <>
+                                    <SongPreview index={0} isDuration={false} isPlays={false} feats={[]} id="1" isIndex={false} isCover title="4x4" artist="Travis Scott" duration="0" plays={0} />
+                                    <SongPreview index={0} isDuration={false} isPlays={false} feats={[]} id="1" isIndex={false} isCover title="4x4" artist="Travis Scott" duration="0" plays={0} />
+                                    <SongPreview index={0} isDuration={false} isPlays={false} feats={[]} id="1" isIndex={false} isCover title="4x4" artist="Travis Scott" duration="0" plays={0} />
+                                    <SongPreview index={0} isDuration={false} isPlays={false} feats={[]} id="1" isIndex={false} isCover title="4x4" artist="Travis Scott" duration="0" plays={0} />
+                                    <SongPreview index={0} isDuration={false} isPlays={false} feats={[]} id="1" isIndex={false} isCover title="4x4" artist="Travis Scott" duration="0" plays={0} />
+                                </>
+                            )}
+                        </div>
+                    </motion.div>
+                    <div className="flex items-center px-3 absolute z-[103] gap-x-2 left-0 font-medium pointer-events-none w-full top-0 bottom-0 my-auto h-max">
+                        <Search size={20} className="opacity-40 min-w-[20px] stroke-3" />
+                        <motion.p
+                            animate={{
+                                opacity: search.length > 0 ? 0 : 1,
+                                y: search.length > 0 ? -10 : 0,
+                            }}
+                            className="text-white/40 truncate"
+                        >
+                            Search music, artists, albums, and playlists.
+                        </motion.p>
                     </div>
-                </motion.div>
-            </motion.div>
-        </main>
+                </div>
+                {!mobile && (
+                    <motion.button initial={false} animate={{
+                        x: isSearching ? 0 : 100,
+                        width: isSearching ? "100px" : "0px",
+                        paddingLeft: isSearching ? "1rem" : "0px",
+                        paddingRight: isSearching ? "1rem" : "0px",
+                        marginLeft: isSearching ? "1.25rem" : "0px",
+                        opacity: isSearching ? 1 : 0,
+                    }} transition={{
+                        ease: "easeInOut",
+                    }} className=" font-medium  hover:bg-zinc-800/80 z-[100] rounded-xl h-[3rem] cursor-pointer flex items-center justify-center">
+                        Cancle
+                    </motion.button>
+                )}
+            </div>
+            <AnimatePresence mode="wait">
+                {search.length === 0 && (
+                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="w-full mt-6 grid  grid-cols-2 z-0 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 auto-rows-fr">
+                        <PopularCover title="RAP" artist="travis" />
+                        <PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" /><PopularCover title="R&B" artist="sza" />
+                        <PopularCover title="TRAP" artist="carti" />
+                        <PopularCover title="DISCO POLO" artist="zenek" />
+                    </motion.div>
+                )}
+                {search.length > 0 && (
+                    <motion.div>
+                        <div className=" w-full flex h-[20rem] gap-10">
+                            <BestResult type={searchResults.results?.[0]?.data_type || 'album'} title={searchResults.results?.[0]?.title} username={searchResults.results?.[0]?.username} artistId={searchResults.results?.[0]?.artist} cover={searchResults.results?.[0]?.image} songs={searchResults.results?.[0]?.songs} />
+                            <TopSongs songs={searchResults.results
+                                .filter((song) => song.data_type === 'song')
+                                .map(song => ({
+                                    id: song.id,
+                                    feats: song.feats || [],
+                                    title: song.title,
+                                    plays: song.plays || 0,
+                                    duration: song.duration || '',
+                                    artist: typeof song.artist === 'number' ? song.artist : 0,
+                                    username: song.username
+                                }))}
+                            />
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </main >
     );
 }
--- components/song-preview.tsx
+++ components/song-preview.tsx
@@ -55,10 +55,11 @@ const PlayAnimation = ({ isPlaying }: { isPlaying: boolean }) => {
   );
 };
 
-export const SongPreview = ({ index, title, artist, feats, plays, duration, isCover, id }: { index: number, title: string, artist: string, feats: string[], plays: number, duration: string, isCover: boolean, id: string }) => {
+export const SongPreview = ({ index, title, artist, feats, plays, duration, isCover, id, isIndex, isPlays, isDuration, artistId }: { index: number, title: string, artist?: string, feats: string[], plays: number, duration: string, isCover: boolean, id: string, isIndex: boolean, isPlays: boolean, isDuration: boolean, artistId?: number }) => {
 
   const [songCover, setSongCover] = useState<string | null>(null);
   const [featsState, setFeatsState] = useState<Array<string> | null>(null);
+  const [artistName, setArtistName] = useState<string | null>(null);
   const setCurrentSongID = useCurrentSongStore((state) => state.setCurrentSongID);
   const currentSongID = useCurrentSongStore((state) => state.currentSongID);
   const setAction = useCurrentSongStore((state) => state.setAction);
@@ -87,61 +88,106 @@ export const SongPreview = ({ index, title, artist, feats, plays, duration, isCo
         setFeatsState(featNames);
       }
     }
+
+    const fetchArtistName = async () => {
+      if (!artistId) return;
+      try {
+        const res = await axios.get(`http://127.0.1:8000/api/artists/${artistId}/`);
+        setArtistName(res.data.username);
+        console.log(res.data.username);
+      } catch (err) {
+        console.error("Error fetching artist data:", err);
+      }
+    }
+    fetchArtistName();
     fetchFeats();
   }, [feats]);
   return (
     <div onClick={() => {
       setCurrentSongID(id.toString());
-    }} className="w-full flex hover:bg-white/5 transition-colors cursor-pointer group items-center rounded-xl md:py-3 py-2 px-2 md:px-0">
-      <div className={`w-full hidden md:block ${isCover ? 'max-w-[40px]' : 'max-w-[65px]'} text-center text-lg font-medium`}>
+    }} className="w-full flex hover:bg-white/5 relative transition-colors cursor-pointer group items-center rounded-xl md:py-2 py-2 md:px-2">
+      <div className={`w-full hidden md:block ${isCover ? isIndex ? 'max-w-[40px]' : 'max-w-[0px]' : 'max-w-[65px]'} text-center text-lg font-medium`}>
         {currentSongID === id.toString() && (
           <PlayAnimation isPlaying={action === "Play"} />
         )}
-        <span className={` group-hover:hidden ${currentSongID === id.toString() ? 'hidden' : 'block'}`}>
-          {index + 1}
-        </span>
-        <button onClick={() => {
-          setCurrentSongID(id.toString());
-          if (currentSongID === id.toString()) {
-            if (action === "Play") {
-              setAction("Pause");
+        {isIndex && (
+          <span className={` group-hover:hidden ${currentSongID === id.toString() ? 'hidden' : 'block'}`}>
+            {index + 1}
+          </span>
+        )}
+        {isIndex && (
+          <button onClick={() => {
+            setCurrentSongID(id.toString());
+            if (currentSongID === id.toString()) {
+              if (action === "Play") {
+                setAction("Pause");
+              } else {
+                setAction("Play");
+              }
             } else {
               setAction("Play");
             }
-          } else {
-            setAction("Play");
-          }
-        }} className={` hover:scale-105 w-full group-hover:flex active:scale-95 transition-all ${currentSongID === id.toString() && action === "Pause" ? 'flex' : 'hidden'} cursor-pointer rounded-full items-center justify-center`}>
-          {currentSongID === id.toString() ? (
-            action === "Play" ? (
-              <Pause className=" text-white md:size-[24px] size-[20px]"
-                fill="white" />
+          }} className={` hover:scale-105 w-full group-hover:flex active:scale-95 transition-all ${currentSongID === id.toString() && action === "Pause" ? 'flex' : 'hidden'} cursor-pointer rounded-full items-center justify-center`}>
+            {currentSongID === id.toString() ? (
+              action === "Play" ? (
+                <Pause className=" text-white md:size-[24px] size-[20px]"
+                  fill="white" />
+              ) : (
+                <Play className=" text-white md:size-[24px] size-[20px]"
+                  fill="white" />
+              )
             ) : (
-              <Play className=" text-white md:size-[24px] size-[20px]"
-                fill="white" />
-            )
-          ) : (
-            <Play
-              className=" text-white md:size-[24px] size-[20px]"
-              fill="white"
-            />
-          )}
-        </button>
+              <Play
+                className=" text-white md:size-[24px] size-[20px]"
+                fill="white"
+              />
+            )}
+          </button>
+        )}
       </div>
       {songCover && (
-        <Image
-          unoptimized
-          src={songCover}
-          alt={title}
-          width={50}
-          height={50}
-          className="rounded-lg w-12 h-12 md:w-12 md:h-12 object-cover mr-3"
-        />)}
+        <div className=" max-w-[48px] w-full relative mr-3">
+          {!isIndex && (<button onClick={() => {
+            setCurrentSongID(id.toString());
+            if (currentSongID === id.toString()) {
+              if (action === "Play") {
+                setAction("Pause");
+              } else {
+                setAction("Play");
+              }
+            } else {
+              setAction("Play");
+            }
+          }} className={` hover:scale-105  group-hover:flex absolute top-0 left-0 w-max h-max right-0 mx-auto bottom-0 my-auto active:scale-95 transition-all ${currentSongID === id.toString() && action === "Pause" ? 'flex' : 'hidden'} cursor-pointer rounded-full items-center justify-center`}>
+            {currentSongID === id.toString() ? (
+              action === "Play" ? (
+                <Pause className=" text-white md:size-[24px] size-[20px]"
+                  fill="white" />
+              ) : (
+                <Play className=" text-white md:size-[24px] size-[20px]"
+                  fill="white" />
+              )
+            ) : (
+              <Play
+                className=" text-white md:size-[24px] size-[20px]"
+                fill="white"
+              />
+            )}
+          </button>
+          )}
+          <Image
+            unoptimized
+            src={songCover}
+            alt={title}
+            width={50}
+            height={50}
+            className="rounded-lg w-12 h-12 md:w-12 md:h-12 object-cover"
+          /></div>)}
       <div className=" w-full flex flex-col">
         <h1 className=" font-semibold md:text-lg text-md truncate">{title}</h1>
         <p className=" text-white/50 text-xs w-max cursor-pointer group-hover:text-white transition-colors font-medium">
           <span className=" hover:underline">
-            {artist}
+            {artistName ? artistName : artist}
           </span>
           {featsState && featsState.length > 0 && (
             <span className="text-white/50 hover:underline group-hover:text-white transition-colors text-xs">
@@ -151,24 +197,31 @@ export const SongPreview = ({ index, title, artist, feats, plays, duration, isCo
           )}
         </p>
       </div>
-      <div className=" w-full hidden lg:block max-w-[400px] group-hover:text-white transition-colors text-center text-white/50 font-medium">
-        {String(plays).replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
-      </div>
-      <button className=" group-hover:opacity-100 md:block hidden opacity-0 transition-opacity w-max">
+      {isPlays && (
+        <div className=" w-full hidden lg:block max-w-[400px] group-hover:text-white transition-colors text-center text-white/50 font-medium">
+          {String(plays).replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+        </div>
+      )}
+      <button className={` group-hover:opacity-100 md:block ${!isDuration && 'mr-3'} hidden opacity-0 transition-opacity w-max`}>
         <CirclePlus
           className=" text-white/50 group-hover:text-white transition-colors"
           size={20}
         />
       </button>
-      <div className=" w-full md:block hidden font-medium text-white/50 transition-colors group-hover:text-white max-w-[150px] text-center">
-        {duration.startsWith('00:') ? duration.substring(3) : duration}
-      </div>
-      <button>
-        <Ellipsis
-          className=" text-white/50 group-hover:text-white transition-colors md:hidden"
-          size={20}
-        />
-      </button>
+      {isDuration && (
+        <>
+          <div className=" w-full md:block hidden font-medium text-white/50 transition-colors group-hover:text-white max-w-[150px] text-center">
+            {duration.startsWith('00:') ? duration.substring(3) : duration}
+          </div>
+
+          <button>
+            <Ellipsis
+              className=" text-white/50 group-hover:text-white transition-colors md:hidden"
+              size={20}
+            />
+          </button>
+        </>
+      )}
     </div >
   );
 };
